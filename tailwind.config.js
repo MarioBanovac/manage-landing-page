f2a11fffd87848e1a42e9e394d57f8fa8aba18f5
MarioBanovac <marioban3@gmail.com>
2022-07-26 17:29:18 +0200

    added navbar without hamburger menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,18 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
       fontFamily: {
         vietnam: ["Be Vietnam Pro", "sans-serif"],
       },
     },
-    colors: {
-      brightRed: "hsl(12, 88%, 59%)",
-      darkBlue: "hsl(228, 39%, 23%)",
-      darkGrayishBlue: "hsl(227, 12%, 61%)",
-      veryDarkBlue: "hsl(233, 12%, 13%)",
-      veryPaleRed: "hsl(13, 100%, 96%)",
-      veryLightGray: "hsl(0, 0%, 98%)",
-    },
-    extend: {},
   },
   plugins: [],
 };
