08adfdeaa786c24467956a1480ba8e40bcd5b3b3
MarioBanovac <marioban3@gmail.com>
2022-07-30 13:39:50 +0200

    hover states added

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         darkBlue: "hsl(228, 39%, 23%)",
         darkGrayishBlue: "hsl(227, 12%, 61%)",
         veryDarkBlue: "hsl(233, 12%, 13%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
         veryPaleRed: "hsl(13, 100%, 96%)",
         veryLightGray: "hsl(0, 0%, 98%)",
       },
